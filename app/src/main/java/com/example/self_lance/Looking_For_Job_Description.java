7531730322f21029ad08a0d9e6ad2a7128d14cd4
yuvis0717 <yuvrajchavda98@gmail.com>
2020-09-15 03:46:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/self_lance/Looking_For_Job_Description.java
@@ -0,0 +1,14 @@
+package com.example.self_lance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Looking_For_Job_Description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looking__for__job__description);
+    }
+}
---
+++ app/src/main/java/com/example/self_lance/Looking_for_job.java
@@ -0,0 +1,14 @@
+package com.example.self_lance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Looking_for_job extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_looking_for_job);
+    }
+}
